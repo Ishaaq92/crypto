cb94fffd00a20dc0819ed48e14b5429a3cb65563
Ishaaq <a.ishaaq@icloud.com>
2024-10-29 12:57:26 +0000

    Initial Commit

---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by Ishaaq Ahmed on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cryptoTests/cryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoTests.swift
+//  cryptoTests
+//
+//  Created by Ishaaq Ahmed on 29/10/2024.
+//
+
+import Testing
+@testable import crypto
+
+struct cryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
